ced61538efd427c4593d03adbaf3ebebd5799cd2
Gabriel Bac <gabriel.bac24@imperial.ac.uk>
2025-11-19 17:30:24 +0000

    Added hamburger for small devices.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'brand-blue': '#1E293B',
+                'brand-gold': '#C5A059',
+                'brand-gray': '#F3F4F6',
+            },
+            fontFamily: {
+                serif: ['"Playfair Display"', 'serif'],
+                sans: ['"Inter"', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+};
